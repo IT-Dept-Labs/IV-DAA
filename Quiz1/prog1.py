bc9a056aea2b7fd94a2a1dac0b994703eb5fc6df
tilaknshenoy <tilaknshenoy@gmail.com>
2018-01-29 14:17:55 +0530

    Quiz1

--- Quiz1/prog1.py
+++ Quiz1/prog1.py
@@ -47,23 +47,23 @@ print(getx(arr,0,n-1))
 
 
 
-# def getVal(arr,n):
-# 	mid=(n-1)//2
-# 	if mid>=0:
-# 		if arr[0]==0:
-# 			return 0
-# 		if arr[n-1]==n-1:
-# 			return n-1
-# 		if mid==arr[mid]:
-# 			return arr[mid]
-# 		elif mid>arr[mid]:
-# 			return getVal(arr[mid:],len(arr[mid:]))
-# 		elif mid<arr[mid]:
-# 			return getVal(arr[:mid],len(arr[:mid]))
-# 		else:
-# 			return -1
-# 	else:
-# 		return -1
-#
-# print(getVal(arr,n))
+def getVal(arr,n):
+	mid=(n-1)//2
+	if mid>=0:
+		if arr[0]==0:
+			return 0
+		if arr[n-1]==n-1:
+			return n-1
+		if mid==arr[mid]:
+			return arr[mid]
+		elif mid>arr[mid]:
+			return getVal(arr[mid:],len(arr[mid:]))
+		elif mid<arr[mid]:
+			return getVal(arr[:mid],len(arr[:mid]))
+		else:
+			return -1
+	else:
+		return -1
+
+print(getVal(arr,n))
 # print(getVal(arr1,n))
