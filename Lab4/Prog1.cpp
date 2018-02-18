d076d75e0af31119da27f72ab00182d2adbb8a21
Tilak N Shenoy <tilaknshenoy@gmail.com>
2018-02-19 01:25:10 +0530

    Lab4

--- Lab4/Prog1.cpp
+++ Lab4/Prog1.cpp
@@ -2,15 +2,29 @@
 using namespace std;
 
 int getMax(int arr[],int n){
-	int sum=arr[0];
+	int max=0,here=0;
+	int indI=n,indJ=-1;
 	for (int i=0;i<n;i++){
-
+			here+=arr[i];
+			if(arr[i]>0 && indI>i )
+				indI=i;
+			if(here<0)
+				here=0;
+			if(here>max){
+				max=here;
+				indJ=i;
+			}
 	}
+	if(indJ==-1)
+		indJ=n-1;
+	cout<<indI<<'\t'<<indJ<<'\n';
+	return max;
 }
 
 
 int main(){
-	int arr[]={-2,-1,1,2,3,4,5,76,10,30};
-	int n=9;
+	int arr[]={-2,-1,-1,-2,3,4,-5,-76,-10,-30};
+	int n=10;
+	cout<<getMax(arr,n)<<'\n';
 
 }
