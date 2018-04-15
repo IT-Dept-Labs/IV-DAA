929a47ac750d03a9c20b0de8ff7103493ecee97b
Tilak N Shenoy <tilaknshenoy@gmail.com>
2018-04-15 22:09:38 +0530

    Lab2

--- Lab2/peak.cpp
+++ Lab2/peak.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 int getPeak(int arr[], int high, int low){
 	
-	int mid=high/low;
+	int mid=(high+low)/2;
+	if (mid==0)
+		return 0;
 	if (arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1])
 		return arr[mid];
 	else if(arr[mid-1]<arr[mid])
@@ -14,13 +16,14 @@ int getPeak(int arr[], int high, int low){
 }
 
 int main(){
-	cout<<"Enter the number of elements";
+	cout<<"Enter the number of elements: ";
 	int n;
 	cin>>n;
 	int arr[n];
-	cout<<"Enter a sequence of integers that first strictly increase and then strictly decrease";
+	cout<<"Enter a sequence of integers that first strictly increase and then strictly decrease: ";
 	for(int i=0;i<n;i++){
 		cin>>arr[i];
 	}
+	
 	cout<<"\n"<<getPeak(arr,0,n)<<"\n";
 }
--- Lab2/peak.py
+++ Lab2/peak.py
@@ -1,15 +1,19 @@
 def getPeak(arr):
 	n=len(arr)
+	mid=n//2
 	if len(arr)==1:
-		print( arr[0])
-	elif arr[n//2]>arr[n//2-1]:
+		print(arr[0])
+	elif arr[mid]>arr[mid+1] and arr[mid]>arr[mid-1]:
+		print( arr[mid])
+	elif arr[mid-1]<arr[mid]:
 		return getPeak(arr[n//2:])
 	else:
-		return getPeak(arr[:arr[n//2]])
+		return getPeak(arr[:n//2])
 
 
 def main():
-	arr=[2,4,6,8,7,5,3]
+	print("Enter a sequence of integers that first strictly increase and then strictly decrease: ")
+	arr=[int(_) for _ in input().split()]
 	getPeak(arr)
 
 if __name__ == '__main__':
